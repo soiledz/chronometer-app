9240c7992fcfb246dd0437e39cf9db10b13f5c50
varik <soiledz@gmail.com>
2026-02-19 23:35:50 +0200

    Fix SQLite for Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,19 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined }
 
+const dbUrl = process.env.DATABASE_URL || 'file:./database.db'
+
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasources: {
+      db: {
+        url: dbUrl
+      }
+    },
     log: ['error'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+
+export default db
